bdca15f93e3930ea411d596d5cff2b5d4002053a
Felix H. Dahlke <fhd@ubercode.de>
2011-06-18 16:33:21 +0200

    Improved stack push/pop test case.

--- public/test/dataStructures/stack.js
+++ public/test/dataStructures/stack.js
@@ -8,10 +8,12 @@ test("empty", function() {
     ok(!stack.stackEmpty(), "The stack shouldn't be empty after a push");
 });
 
-test("pop", function() {
-    expect(2);
+test("push,pop", function() {
+    expect(3);
     var stack = new Stack();
     stack.push(1);
+    stack.push(2);
+    equal(stack.pop(), 2);
     equal(stack.pop(), 1);
     equal(stack.pop(), null);
 });
