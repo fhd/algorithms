a9d79267ef19fc1a7cce3c7e6804740a9644018c
Felix H. Dahlke <fhd@ubercode.de>
2012-04-14 08:30:42 +0200

    Add Sinon.JS and use mocks for the utilities test

--- public/test/utilities.js
+++ public/test/utilities.js
@@ -1,23 +1,21 @@
 module("utilities");
 
-test("createDrawFunction", 1, function() {
-    var mockContextUsed = false,
-        mockCanvas = {
-            width: 100,
-            height: 100,
+test("createDrawFunction", function() {
+    var context = {
+            fillRect: function() {}
+        },
+        canvas = {
             getContext: function() {
-                return {
-                    fillRect: function() {
-                        mockContextUsed = true;
-                    }
-                };
+                return context;
             }
-        };
+        },
+        mock = sinon.mock(context);
 
-    createDrawFunction(mockCanvas, function(context, width, height) {
+    mock.expects("fillRect").once();
+
+    createDrawFunction(canvas, function(context, width, height) {
         context.fillRect();
     })();
 
-    ok(mockContextUsed,
-       "The draw function should use the context of the supplied canvas.");
+    mock.verify();
 });
