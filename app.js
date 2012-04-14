66722db88a914c7ddebd432178f4e78fd0c06dd1
Felix H. Dahlke <fhd@ubercode.de>
2012-04-14 07:48:15 +0200

    Move algorithm specifications to their own file

--- app.js
+++ app.js
@@ -1,129 +1,40 @@
-var categories = {
-    sorting: {
-        name: "Sorting",
-        algorithms: {
-            insertionSort: {
-                name: "Insertion Sort",
-                description: "A simple sort algorithm, works like most people "
-                    + "sort cards on their hand."
-            },
-            mergeSort: {
-                name: "Merge Sort",
-                description: "A fast, recursive sort algorithm that divides "
-                    + "and merges the input."
-            },
-            bubbleSort: {
-                name: "Bubble Sort",
-                description: "An extremely simple sort algorithm with so "
-                    + "abysmal performance that this demonstration doesn't "
-                    + "show every step."
-            },
-            heapsort: {
-                name: "Heapsort",
-                description: "A well performing sort algorithm that uses a "
-                    + "binary heap."
-            },
-            quicksort: {
-                name: "Quicksort",
-                description: "A fast, recursive sort algorithm that partitions "
-                    + "the input.",
-            },
-            randomizedQuicksort: {
-                name: "Randomized Quicksort",
-                description: "A variant of "
-                    + "<a href=\"/quicksort\">Quicksort</a> that avoids the "
-                    + "worst case running time by partitioning around a random "
-                    + "pivot element."
-            }
-        }
-    },
-    dataStructures: {
-        name: "Data Structures",
-        algorithms: {
-            stack: {
-                name: "Stack",
-                description: "A simple data structure that stores elements as "
-                    + "on a physical stack: The first element inserted is the "
-                    + "last to be extracted (FILO)."
-            },
-            queue: {
-                name: "Queue",
-                description: "A simple data structure that stores elements as "
-                    + "on a physical queue: The first element inserted is the "
-                    + "first to be extracted (FIFO)."
-            },
-            linkedList: {
-                name: "Linked List",
-                description: "A data structure that stores a dynamic number of "
-                    + "elements."
-            }
-        }
-    }
-},
-    algorithms = createAlgorithms(categories);
+var algorithms = require("./lib/algorithms.js"),
+    express = require("express"),
+    app = module.exports = express.createServer(),
+    fs = require("fs");
 
-function createAlgorithms(categories) {
-    var algorithms = {};
+app.configure(function() {
+    app.set("views", __dirname + "/views");
+    app.set("view engine", "jade");
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + "/public"));
+});
 
-    Object.keys(categories).forEach(function (categoryKey) {
-        var category = categories[categoryKey];
-        Object.keys(category.algorithms).forEach(function(algorithmKey) {
-            var algorithm = category.algorithms[algorithmKey];
-            algorithm.category = categoryKey;
-            algorithms[algorithmKey] = algorithm;
+app.configure("development", function() {
+    app.use(express.errorHandler({dumpExceptions: true, showStack: true}));
+});
+
+app.configure("production", function() {
+    app.use(express.errorHandler());
+});
+
+app.get("/", function(req, res) {
+    res.render("index", {"categories": algorithms.categories});
+});
+
+app.get("/:algorithm", function(req, res) {
+    var algorithm = algorithms.algorithms[req.params.algorithm];
+    if (algorithm) {
+        algorithm.code = fs.readFileSync("public/js/" + algorithm.category
+                                         + "/" + algorithm.file);
+        res.render(algorithm.category, {
+            currentAlgorithm: algorithm,
+            "categories": algorithms.categories
         });
-    });
+    } else
+        res.send(404);
+});
 
-    Object.keys(algorithms).forEach(function (key) {
-        var value = algorithms[key];
-        value.file = key + ".js";
-        value.functionName = key;
-        value.url = "/" + key;
-    });
-
-    return algorithms;
-}
-
-function startServer() {
-    var express = require("express"),
-        app = module.exports = express.createServer(),
-        fs = require("fs");
-
-    app.configure(function() {
-        app.set("views", __dirname + "/views");
-        app.set("view engine", "jade");
-        app.use(express.bodyParser());
-        app.use(express.methodOverride());
-        app.use(app.router);
-        app.use(express.static(__dirname + "/public"));
-    });
-
-    app.configure("development", function() {
-        app.use(express.errorHandler({dumpExceptions: true, showStack: true}));
-    });
-
-    app.configure("production", function() {
-        app.use(express.errorHandler());
-    });
-
-    app.get("/", function(req, res) {
-        res.render("index", {"categories": categories});
-    });
-
-    app.get("/:algorithm", function(req, res) {
-        var algorithm = algorithms[req.params.algorithm];
-        if (algorithm) {
-            algorithm.code = fs.readFileSync("public/js/" + algorithm.category
-                                             + "/" + algorithm.file);
-            res.render(algorithm.category, {
-                currentAlgorithm: algorithm,
-                "categories": categories
-            });
-        } else
-            res.send(404);
-    });
-
-    app.listen(3000);
-}
-
-startServer();
+app.listen(3000);
