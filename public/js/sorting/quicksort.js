8739ed73c86a88b45c3bf7431420e4ae5c1ecb6f
Felix H. Dahlke <fhd@ubercode.de>
2012-04-17 09:31:50 +0200

    Use the module pattern for the sort algorithms

--- public/js/sorting/quicksort.js
+++ public/js/sorting/quicksort.js
@@ -1,35 +1,37 @@
-function exchange(array, i, j) {
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-}
-
-function partition(array, start, end) {
-    var last = end - 1,
-        pivot = array[last],
-        i = start - 1,
-        j;
-
-    for (j = start; j < last; j++)
-        if (array[j] <= pivot)
-            exchange(array, ++i, j);
-
-    exchange(array, ++i, last);
-    return i;
-}
-
-function quicksort(array, start, end) {
-    var split;
-
-    if (start == undefined)
-        start = 0;
-    if (end == undefined)
-        end = array.length;
-
-    if (start < end - 1) {
-        split = partition(array, start, end);
-        update(array);
-        quicksort(array, start, split);
-        quicksort(array, split + 1, end);
+var quicksort = (function() {
+    function exchange(array, i, j) {
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
-}
+
+    function partition(array, start, end) {
+        var last = end - 1,
+            pivot = array[last],
+            i = start - 1,
+            j;
+
+        for (j = start; j < last; j++)
+            if (array[j] <= pivot)
+                exchange(array, ++i, j);
+
+        exchange(array, ++i, last);
+        return i;
+    }
+
+    return function(array, start, end) {
+        var split;
+
+        if (start == undefined)
+            start = 0;
+        if (end == undefined)
+            end = array.length;
+
+        if (start < end - 1) {
+            split = partition(array, start, end);
+            update(array);
+            quicksort(array, start, split);
+            quicksort(array, split + 1, end);
+        }
+    };
+})();
--- public/js/sorting/randomizedQuicksort.js
+++ public/js/sorting/randomizedQuicksort.js
@@ -1,41 +1,43 @@
-function exchange(array, i, j) {
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-}
-
-function partition(array, start, end) {
-    var last = end - 1,
-        pivot = array[last],
-        i = start - 1,
-        j;
-
-    for (j = start; j < last; j++)
-        if (array[j] <= pivot)
-            exchange(array, ++i, j);
-
-    exchange(array, ++i, last);
-    return i;
-}
-
-function randomizedPartition(array, start, end) {
-    var randomPivot = Math.floor(Math.random() * (end - start) + start);
-    exchange(array, end - 1, randomPivot);
-    return partition(array, start, end);
-}
-
-function randomizedQuicksort(array, start, end) {
-    var split;
-
-    if (start == undefined)
-        start = 0;
-    if (end == undefined)
-        end = array.length;
-
-    if (start < end - 1) {
-        split = randomizedPartition(array, start, end);
-        update(array);
-        randomizedQuicksort(array, start, split);
-        randomizedQuicksort(array, split + 1, end);
+var randomizedQuicksort = (function() {
+    function exchange(array, i, j) {
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
-}
+
+    function partition(array, start, end) {
+        var last = end - 1,
+            pivot = array[last],
+            i = start - 1,
+            j;
+
+        for (j = start; j < last; j++)
+            if (array[j] <= pivot)
+                exchange(array, ++i, j);
+
+        exchange(array, ++i, last);
+        return i;
+    }
+
+    function randomizedPartition(array, start, end) {
+        var randomPivot = Math.floor(Math.random() * (end - start) + start);
+        exchange(array, end - 1, randomPivot);
+        return partition(array, start, end);
+    }
+
+    return function(array, start, end) {
+        var split;
+
+        if (start == undefined)
+            start = 0;
+        if (end == undefined)
+            end = array.length;
+
+        if (start < end - 1) {
+            split = randomizedPartition(array, start, end);
+            update(array);
+            randomizedQuicksort(array, start, split);
+            randomizedQuicksort(array, split + 1, end);
+        }
+    };
+})();
