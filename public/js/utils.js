6bba11bedc626cc2a972f8dcf56eb75bbba5679c
Felix H. Dahlke <fhd@ubercode.de>
2012-04-17 18:09:04 +0200

    Avoid error message in tests

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,6 +1,8 @@
 var utils = {
     createDrawFunction: function(canvas, drawFunction) {
         return function() {
+            if (!canvas)
+                return;
             drawFunction(canvas.getContext("2d"), canvas.width, canvas.height);
         };
     }
